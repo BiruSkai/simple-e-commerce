f543de8b19d1e934c2af9e91fcbf070317cb38c8
Nikolaus Albert <115626969+BiruSkai@users.noreply.github.com>
2024-06-27 16:25:00 +0700

    db folder finished

--- server/db/index.js
+++ server/db/index.js
@@ -2,10 +2,13 @@ const {fetchUserByEmailDb, fetchUserByGoogleIdDb, createUserDb, addGoogleIdUserD
         , fetchUsersDb, fetchUserByIdDb} = require("./users_db");
 const {createCartDb, fetchCartsDb, fetchCartByIdDb, createProductInCartDb, modifyCartDb, 
         removeCartProductDb, removeCartDb} = require("./carts_db")
+const {fetchProductsDb, fetchProductByIdDb, createProductDb, modifyProductDb, removeProductDb} = require("./products_db")
+const {fetchOrdersDb,  fetchOrderByIdDb, fetchOrdersByUserDb, createOrderDb, createProductInOrderDb} = require("./orders_db")
 
 
 module.exports = {
-        fetchUserByEmailDb, fetchUserByGoogleIdDb, createUserDb, addGoogleIdUserDb,
-        modifyUserDb, fetchUsersDb, fetchUserByIdDb,
-        fetchCartsDb, fetchCartByIdDb, createCartDb, createProductInCartDb, modifyCartDb, removeCartProductDb, removeCartDb
+        fetchUserByEmailDb, fetchUserByGoogleIdDb, createUserDb, addGoogleIdUserDb, modifyUserDb, fetchUsersDb, fetchUserByIdDb,
+        fetchCartsDb, fetchCartByIdDb, createCartDb, createProductInCartDb, modifyCartDb, removeCartProductDb, removeCartDb,
+        fetchProductsDb, fetchProductByIdDb, createProductDb, modifyProductDb, removeProductDb,
+        fetchOrdersDb,  fetchOrderByIdDb, fetchOrdersByUserDb, createOrderDb, createProductInOrderDb
 }
