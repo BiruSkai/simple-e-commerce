bd21446a881071578ff5c26f678d403f2adcd6a6
Nikolaus Albert <115626969+BiruSkai@users.noreply.github.com>
2024-06-28 18:31:40 +0700

    services folder finished

--- server/db/carts_db.js
+++ server/db/carts_db.js
@@ -14,7 +14,7 @@ const fetchCartsDb = async () => {
 //Fetches all products in a cart for a userId
 const fetchCartByIdDb = async (userId) => {
         const res = await pool.query(
-                `SELECT products.id, name, description, category, image_url, status, quantity FROM carts
+                `SELECT products.id, name, price, description, category, image_url, status, quantity FROM carts
                 INNER JOIN cart_products ON carts.id = cart_products.cart_id
                 INNER JOIN products ON cart_products.product_id = products.id
                 WHERE user_id = $1`, [userId]
--- server/services/index.js
+++ server/services/index.js
@@ -1,10 +1,14 @@
 const usersService = require("./users_service");
 const authService = require("./auth_service");
 const cartService = require("./cart_service");
+const productsService = require("./products_service");
+const ordersService = require("./orders_service");
 
 
 module.exports = {
         usersService,
         authService,
-        cartService
+        cartService,
+        productsService,
+        ordersService
 }
